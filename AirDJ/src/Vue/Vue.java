4670afd5c2eafd4eff0bb5925d0ee396056f4441
FTMT <FTMT@PORTABLE10.crif.local>
2012-12-21 15:28:23 +0100

    icon

--- AirDJ/src/Vue/Vue.java
+++ AirDJ/src/Vue/Vue.java
@@ -3,6 +3,7 @@ package Vue;
 import java.awt.BorderLayout;
 import java.awt.Color;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
@@ -27,17 +28,18 @@ public class Vue extends JFrame
 		setSize(800, 600);
 		setLocationRelativeTo(null);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setIconImage(new ImageIcon("image/icon.png").getImage());
 		setVisible(true);
-		
+
 		BorderLayout borderLayout = new BorderLayout();
 		borderLayout.setHgap(5);
 		borderLayout.setVgap(5);
-		
+
 		JPanel panel = new JPanel();
 		panel.setBackground(new Color(0x262221));
 		panel.setLayout(borderLayout);
 		getContentPane().add(panel);
-		
+
 		panel.add(vueBrowser, BorderLayout.NORTH);
 		panel.add(vueCrossfinder, BorderLayout.CENTER);
 		panel.add(vueKinect, BorderLayout.SOUTH);
