c4296c03f884dd37d6310e79917d4cb268f06434
ameyer <arthur.meyer@telecom-paristech.fr>
2013-01-21 16:43:44 +0100

    Petit changment commentaire

--- projet-2012-54/src/Audio/IPlayer.java
+++ projet-2012-54/src/Audio/IPlayer.java
@@ -8,6 +8,6 @@ public interface IPlayer
 	public void setVitesse(int vitesse); //vitesse entiere positive: a modifier
 	public float getPosition(); //retourne la position en seconde
 	public void setPosition(float position); //avance a la position souhaite en seconde
-	public float getCurrentVolume(); //volume du sample en cours
+	public float getCurrentVolume(); //volume du sample en cours: a modifier
 	public float getLength(); //duree totale du morceau
 }
