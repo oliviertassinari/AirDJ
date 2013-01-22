bf4c58aef49cca432cdbc5dd4d05066124fb6124
ameyer <arthur.meyer@telecom-paristech.fr>
2013-01-22 14:14:14 +0100

    Correction de bug

--- projet-2012-54/src/Audio/IPlayer.java
+++ projet-2012-54/src/Audio/IPlayer.java
@@ -5,9 +5,9 @@ public interface IPlayer
 	public void setPause();
 	public void setPlay();
 	public void setVolume(float volume); //volume de 0 a 100
-	public void setVitesse(int vitesse); //vitesse entiere positive: a modifier
+	public void setVitesse(int vitesse); //non fonctionelle
 	public float getPosition(); //retourne la position en seconde
 	public void setPosition(float position); //avance a la position souhaite en seconde
-	public float getCurrentVolume(); //volume du sample en cours: a modifier
+	public float getCurrentVolume(); //volume du sample en cours:en cours de modification
 	public float getLength(); //duree totale du morceau
 }
--- projet-2012-54/src/Audio/Player.java
+++ projet-2012-54/src/Audio/Player.java
@@ -26,19 +26,19 @@ public class Player implements Runnable, IPlayer
 		{
 			//Lecture
 	 		int frameSize = audioFormat.getFrameSize();
-	 		int frameRate = (int) audioFormat.getFrameRate();
-			byte bytes1[] = new byte[5*frameSize*10];
-			byte bytes2[] = new byte[5*frameSize];
+	 		float frameRate = audioFormat.getFrameRate();
+			byte bytes[] = new byte[5*frameSize];
 			int bytesRead=0;
-			int temp1 = 0;
-
-			int bytesDixiemeSeconde = frameSize * frameRate / 10;
-			int t1 = 0;
-			int t2 = 0;
+			int bytesDixiemeSeconde = (int) (frameSize * frameRate / 10);
+			
+			int tmp1 = 0;
+			int tmp2 = 0;
+			int tmp3 = 0;
+			int tmp4 = 0;
 			
 			synchronized(this)
 			{
-				while(((bytesRead = audioInputStream.read(bytes1, 0, 5*frameSize*vitesse)) != -1))
+				while(((bytesRead = audioInputStream.read(bytes, 0, 5*frameSize)) != -1))
 				{
 					while(status != 1)
 					{
@@ -61,23 +61,22 @@ public class Player implements Runnable, IPlayer
 						line.start();
 					}
 					
-					t1 = byteFromBeginning % bytesDixiemeSeconde;
-					if (t1 == 0) {currentVolume = (int) (3*t2); t2 = 0;}
-					byteFromBeginning += 5*frameSize*vitesse;
-					for(int i =0; i<5;i++)
+					tmp3 = byteFromBeginning % bytesDixiemeSeconde;
+					if (tmp3 == 0)
 					{
-						for(int j=0; j<frameSize;j++)
-						{
-							byte temp2 = bytes1[i*frameSize*vitesse+j];
-							Byte temp3 = new Byte(temp2);
-							temp1 =  ((i*frameSize+j)*temp1 + Math.abs(temp3.intValue()))/(i*frameSize+j+1);
-							bytes2[frameSize*i+j] = temp2;
-						}
+						currentVolume = (int) (currentVolume*0.5 + 0.5*3*tmp2);
+						tmp2 = 0;
+					}
+					byteFromBeginning += 5*frameSize;
+					for(int i =0; i<bytes.length;i++)
+					{
+							Byte temp0 = new Byte(bytes[i]);
+							tmp1 += Math.abs(temp0.intValue())/(bytes.length);
 					}
 	
-					line.write(bytes2, 0, bytes2.length);
-					t2 = (t2*t1 +  temp1*5*frameSize)/(t1+5*frameSize);
-					temp1 = 0;
+					line.write(bytes, 0, bytes.length);
+					tmp2 = (tmp2*tmp3 +  tmp1*5*frameSize)/(tmp3+5*frameSize);
+					tmp1 = 0;
 				}
 	
 				line.drain(); //On attend que le buffer soit vide
