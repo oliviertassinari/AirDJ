a6750b8cafb5da30303a2c0127d185345976b137
ameyer <arthur.meyer@telecom-paristech.fr>
2013-02-04 11:51:02 +0100

    EventListener

--- projet-2012-54/src/KinectControle/KinectListener.java
+++ projet-2012-54/src/KinectControle/KinectListener.java
@@ -7,14 +7,14 @@ public class KinectListener implements ListenerInterface {
 	public void ListenToKinect(KinectEvent event) {
 		// TODO Auto-generated method stub
 		if (event.getMessage()=="play"){
-			(event.getSource().getVue().getVuePlayP1()).setPause(0);
-			(event.getSource().getVue().getVuePlayP1()).setPlay(2);
+			//(event.getSource().getVue().getVuePlayP1()).setPause(0);
+			//(event.getSource().getVue().getVuePlayP1()).setPlay(2);
 			(event.getSource().getModele().getModelePlayP1()).setPlay();
 			
 		}
 		if (event.getMessage()=="pause"){
-			(event.getSource().getVue().getVuePlayP1()).setPause(2);
-			(event.getSource().getVue().getVuePlayP1()).setPlay(0);
+			//(event.getSource().getModele().getModelePlayP1()).setPause(2);
+			//(event.getSource().getModele().getModelePlayP1()).setPlay(0);
 			(event.getSource().getModele().getModelePlayP1()).setPause();
 		}
 		(event.getSource().getVue().getVuePlayP1()).repaint();
