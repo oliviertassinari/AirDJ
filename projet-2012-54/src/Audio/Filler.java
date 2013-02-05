85400d93946d975c8baf94a4a1bb4c8c91dfc7c4
ameyer <arthur.meyer@telecom-paristech.fr>
2013-02-05 14:26:53 +0100

    Filler amelioré

--- projet-2012-54/src/Audio/Filler.java
+++ projet-2012-54/src/Audio/Filler.java
@@ -57,37 +57,35 @@ public class Filler implements Runnable {
 			int pos = 0;
 			int i = 0;
 			int ind[] = new int[2];
-			int tmp[][] = new int[2][2];
+			int tmp[] = new int[2];
 			
 			while(((ais.read(bytes, 0, bytes.length)) != -1))
 			{
 				while(i<bytes.length)
 				{
-					for(int j=0;j<5*frameSize;j++)
-					{
-						Byte temp0 = new Byte(bytes[i+j]);
-						tmp[ind[1]][0] += Math.abs(temp0.intValue())/(5*frameSize/2);
-						ind[0] ++;
-						if (ind[0] == 2) ind[1] = 1;
-						if (ind[0] == 4) {ind[0]=0; ind[1] = 0;}
-					}
-					if (tmp[0][0]<10) tmp[0][0]=0;
-					if (tmp[1][0]<10) tmp[1][0]=0;
-					tmp[0][1] = (5*frameSize*tmp[0][0] + tmp[0][1]*i )/(5*frameSize + i);
-					tmp[1][1] = (5*frameSize*tmp[1][0] + tmp[1][1]*i )/(5*frameSize + i);
-					tmp[0][0] = 0;
-					tmp[1][0] = 0;
-					i += 5*frameSize;
+					Byte temp = new Byte(bytes[i]);
+					tmp[ind[1]] += Math.abs(temp.intValue());
+					ind[0] ++;
+					if (ind[0] == 2) ind[1] = 1;
+					if (ind[0] == 4) {ind[0]=0; ind[1] = 0;}
+					i++;
 				}
-				if (tmp[0][1]>tmp[1][1]) tmp[0][1] += 1.5*(tmp[0][1]-tmp[1][1]);
-				if (tmp[1][1]>tmp[0][1]) tmp[1][1] += 1.5*(tmp[1][1]-tmp[0][1]);
-				if (pos == 0) {player.setVolumeArray(tmp[0][0], pos, 0); player.setVolumeArray(tmp[1][0], pos, 1);}
-				if (pos != 0) {player.setVolumeArray((int) (0.5 * player.getVolumeArray(pos-1, 0) + 1.5 * tmp[0][1]), pos, 0);
-								player.setVolumeArray((int) (0.5 * player.getVolumeArray(pos-1, 1) + 1.5 * tmp[1][1]), pos, 1);}
+				tmp[0] = tmp[0]/bytes.length;		
+				tmp[1] = tmp[1]/bytes.length;
+				if (pos == 0) 
+				{
+					player.setVolumeArray(tmp[0], pos, 0); 
+					player.setVolumeArray(tmp[1], pos, 1);
+				}
+				if (pos != 0) 
+				{
+					player.setVolumeArray((int) (0.5 * player.getVolumeArray(pos-1, 0) + 1.5 * tmp[0]), pos, 0);
+					player.setVolumeArray((int) (0.5 * player.getVolumeArray(pos-1, 1) + 1.5 * tmp[1]), pos, 1);
+				}
 				pos ++;
 				i = 0;
-				tmp[0][1] = 0;
-				tmp[1][1] = 0;
+				tmp[0] = 0;
+				tmp[1] = 0;
 				}
 
 		}
