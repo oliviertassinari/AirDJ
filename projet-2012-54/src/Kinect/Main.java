94f2430a3d8812d294e46d97915abd0e07072b82
tassinari <olivier.tassinari@telecom-paristech.fr>
2013-02-06 20:23:15 +0100

    play / pause P1 / P2 : ok

--- projet-2012-54/src/Kinect/Main.java
+++ projet-2012-54/src/Kinect/Main.java
@@ -23,30 +23,11 @@ public class Main
 		Controle controle = new Controle(vue, modele);
 
 		modele.getModelePlayP1().setFilePath("data/Any Colour You Like.wav");
-		
-		//test controleKinect
+
 		KinectListener listener = new KinectListener();
 		final KinectSource kinectSource = new KinectSource(vue, modele);
 		kinectSource.addKinectListener(listener);
-		vue.addKeyListener(new KeyListener() {
-		
-			public void keyPressed(KeyEvent arg0) {
-				// TODO Auto-generated method stub
-							
-				if((arg0.getKeyChar())=='p'){
-					kinectSource.fireEvent("play");
-				}
-				}
 
-			public void keyReleased(KeyEvent arg0) {
-			}
-
-			
-			public void keyTyped(KeyEvent arg0) {
-			}
-            
-        });
-		
 		new Kinect(kinectSource);
 	}
 }
