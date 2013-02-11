e8c457596daa3f0dd196a96c5741cfd319070a7e
Lisa <lisa.bost@telecom-paristech.fr>
2013-02-11 19:56:52 +0100

    ajout images dans affichage commandes kinect.

--- projet-2012-54/src/KinectControle/KinectListener.java
+++ projet-2012-54/src/KinectControle/KinectListener.java
@@ -1,6 +1,7 @@
 package KinectControle;
 
 import Modele.ModeleCrossfinder;
+import Modele.ModeleKinect;
 import Modele.ModelePlay;
 
 public class KinectListener implements ListenerInterface
@@ -11,6 +12,7 @@ public class KinectListener implements ListenerInterface
 	@Override
 	public void ListenToKinect(KinectEvent event)
 	{		
+		ModeleKinect modeleKinect = event.getSource().getModele().getModeleKinect();
 		
 		// commande play/pause
 		if(event.getMessage() == "play")
@@ -31,12 +33,26 @@ public class KinectListener implements ListenerInterface
 				modelePlay.setPlay();
 				modelePlay.setButtonPlay("release");
 				modelePlay.setButtonPause("out");
+				
+				if ( event.getCote()=="left"){
+					modeleKinect.setMessageGauche("play");
+				}
+				else if ( event.getCote()=="right"){
+					modeleKinect.setMessageDroite("play");
+				}
 			}
 			else if(modelePlay.getState() == 1)
 			{
 				modelePlay.setPause();
 				modelePlay.setButtonPlay("out");
 				modelePlay.setButtonPause("release");
+				
+				if ( event.getCote()=="left"){
+					modeleKinect.setMessageGauche("pause");
+				}
+				else if ( event.getCote()=="right"){
+					modeleKinect.setMessageDroite("pause");
+				}
 			}		
 		}
 		
@@ -45,14 +61,18 @@ public class KinectListener implements ListenerInterface
 			ModeleCrossfinder crossfinder = event.getSource().getModele().getModeleCrossfinder();
 			if(event.getCote() == "left"){
 				crossfinder.setVolumeP1(crossfinder.getVolumeP1()+event.getValeur());
+				modeleKinect.setMessageGauche("volume");
 			}
 			else if(event.getCote() == "right"){
 				crossfinder.setVolumeP2(crossfinder.getVolumeP2()+event.getValeur());
+				modeleKinect.setMessageDroite("volume");
 			}		
 		}
 		if(event.getMessage() == "crossfinder"){
 			ModeleCrossfinder crossfinder = event.getSource().getModele().getModeleCrossfinder();
 			crossfinder.setCrossfinder(crossfinder.getCrossfinder()+event.getValeur());
+			modeleKinect.setMessageDroite("crossfinder");
+			modeleKinect.setMessageGauche("crossfinder");
 		}
 			
 		
--- projet-2012-54/src/Vue/VueKinect.java
+++ projet-2012-54/src/Vue/VueKinect.java
@@ -4,8 +4,10 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.Graphics;
+import java.awt.Image;
 import java.awt.image.BufferedImage;
 
+import javax.swing.ImageIcon;
 import javax.swing.JPanel;
 
 public class VueKinect extends JPanel
@@ -16,7 +18,11 @@ public class VueKinect extends JPanel
 { 
 	private Vue vue;
 	private BufferedImage kinectImage;
-	
+	private Image play;
+	private Image pause;
+	private Image volume;
+	private Image crossfinder;
+		
 	
 	
 	
@@ -39,6 +45,10 @@ public class VueKinect extends JPanel
 	 */
 	protected void paintComponent(Graphics g)
 	{
+		play = new ImageIcon("image/playvuekinect.png").getImage();
+		pause = new ImageIcon("image/pausevuekinect.png").getImage();
+		volume = new ImageIcon("image/volumevuekinect.png").getImage();
+		crossfinder = new ImageIcon("image/crossfindervuekinect.png").getImage();
 		
 		String messageDroite=vue.getModele().getModeleKinect().getMessageDroite();
 		String messageGauche=vue.getModele().getModeleKinect().getMessageGauche();
@@ -55,8 +65,33 @@ public class VueKinect extends JPanel
 		g.drawString("Main droite", 780, 25);
 		
 		g.setFont(new Font("sansserif", Font.BOLD, 15));
-		g.drawString(messageGauche, 450, 100);
-		g.drawString(messageDroite, 780, 100);
+		if(messageDroite=="play"){
+			g.drawImage(play,  730, 55,  null);
+		}
+		if(messageDroite=="pause"){
+			g.drawImage(pause,  730, 55,  null);
+		}
+		if(messageDroite=="volume"){
+			g.drawImage(volume,  740, 55,  null);
+		}
+		else if(messageDroite=="crossfinder"){
+			g.drawImage(crossfinder,  745, 55,  null);
+		}
+		
+		if(messageGauche=="play"){
+			g.drawImage(play,  405, 55,  null);
+		}
+		if(messageGauche=="pause"){
+			g.drawImage(pause,  405, 55,  null);
+		}
+		if(messageGauche=="volume"){
+			g.drawImage(volume,  415, 55,  null);
+		}
+		else if(messageGauche=="crossfinder"){
+			g.drawImage(crossfinder,  420, 55,  null);
+		}
+		//g.drawString(messageGauche, 450, 100);
+		//g.drawString(messageDroite, 780, 100);
 		
 		
 	}
