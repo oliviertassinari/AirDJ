12f064a85f7dbd071b6a234c39eb5fcc365a9a17
Arthur-MEYER <Arthur-MEYER@137.194.22.92>
2013-02-12 15:00:23 +0100

    affichage video

--- projet-2012-54/src/Vue/VueKinect.java
+++ projet-2012-54/src/Vue/VueKinect.java
@@ -11,13 +11,13 @@ import javax.swing.ImageIcon;
 import javax.swing.JPanel;
 
 /**
- * Panel devant afficher ce qui est filmé 
- * prend en attribut : 
- * - la vue qui le contient 
- * - une BufferedImage qui est mise à jour périodiquement par la partie kinect pour afficher la video 
- * - 4 images pour décrire les commandes détectées par la partie kinect
+ * Panel devant afficher ce qui est filmé prend en attribut : - la vue qui le
+ * contient - une BufferedImage qui est mise à jour périodiquement par la
+ * partie kinect pour afficher la video - 4 images pour décrire les commandes
+ * détectées par la partie kinect
  */
-public class VueKinect extends JPanel {
+public class VueKinect extends JPanel
+{
 	private Vue vue;
 	private BufferedImage kinectImage;
 	private Image play;
@@ -30,29 +30,26 @@ public class VueKinect extends JPanel {
 	 * 
 	 * @param Vue
 	 */
-	public VueKinect(Vue vue) {
+	public VueKinect(Vue vue)
+	{
 		this.vue = vue;
 
 		setBackground(Color.black);
 		setPreferredSize(new Dimension(1000, 240));
 	}
 
-	protected void paintComponent(Graphics g) {
-		g.drawImage(kinectImage, 10, 10, null);
-
+	protected void paintComponent(Graphics g)
+	{
 		play = new ImageIcon("image/playvuekinect.png").getImage();
 		pause = new ImageIcon("image/pausevuekinect.png").getImage();
 		volume = new ImageIcon("image/volumevuekinect.png").getImage();
-		crossfinder = new ImageIcon("image/crossfindervuekinect.png")
-				.getImage();
+		crossfinder = new ImageIcon("image/crossfindervuekinect.png").getImage();
 
-		String messageDroite = vue.getModele().getModeleKinect()
-				.getMessageDroite();
-		String messageGauche = vue.getModele().getModeleKinect()
-				.getMessageGauche();
+		String messageDroite = vue.getModele().getModeleKinect().getMessageDroite();
+		String messageGauche = vue.getModele().getModeleKinect().getMessageGauche();
 		g.setColor(Color.black);
 		g.fillRect(0, 0, 1000, 240);
-		g.drawImage(kinectImage, 320, 240, null);
+		g.drawImage(kinectImage, 10, 10, null);
 
 		g.setFont(new Font("sansserif", Font.BOLD, 17));
 		g.setColor(Color.WHITE);
@@ -62,27 +59,35 @@ public class VueKinect extends JPanel {
 		g.drawString("Main droite", 780, 25);
 
 		g.setFont(new Font("sansserif", Font.BOLD, 15));
-		if (messageDroite == "play") {
+		if (messageDroite == "play")
+		{
 			g.drawImage(play, 730, 55, null);
 		}
-		if (messageDroite == "pause") {
+		if (messageDroite == "pause")
+		{
 			g.drawImage(pause, 730, 55, null);
 		}
-		if (messageDroite == "volume") {
+		if (messageDroite == "volume")
+		{
 			g.drawImage(volume, 740, 55, null);
-		} else if (messageDroite == "crossfinder") {
+		} else if (messageDroite == "crossfinder")
+		{
 			g.drawImage(crossfinder, 745, 55, null);
 		}
 
-		if (messageGauche == "play") {
+		if (messageGauche == "play")
+		{
 			g.drawImage(play, 405, 55, null);
 		}
-		if (messageGauche == "pause") {
+		if (messageGauche == "pause")
+		{
 			g.drawImage(pause, 405, 55, null);
 		}
-		if (messageGauche == "volume") {
+		if (messageGauche == "volume")
+		{
 			g.drawImage(volume, 415, 55, null);
-		} else if (messageGauche == "crossfinder") {
+		} else if (messageGauche == "crossfinder")
+		{
 			g.drawImage(crossfinder, 420, 55, null);
 		}
 
@@ -90,9 +95,11 @@ public class VueKinect extends JPanel {
 
 	/**
 	 * setters de la BufferedImage qui sera appelé par l'objet Kinect
+	 * 
 	 * @param kinectImage
 	 */
-	public void setKinectImage(BufferedImage kinectImage) {
+	public void setKinectImage(BufferedImage kinectImage)
+	{
 		this.kinectImage = kinectImage;
 		repaint();
 	}
